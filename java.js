cf02678428e9ec8e966b281245fa8d9cfe183c94
Alexandre Nogueira Araújo Lopes <alexandre.nogueira@dinfor.uniceub>
2024-08-27 11:08:43 -0300

    adicionando script

---
+++ java.js
@@ -0,0 +1,8 @@
+let n = prompt(" digite um valor : ")
+if(n > 100){
+    console.log(" esse numero é maior que cem");
+}else if (n < 100){
+    console.log(" esse numero é menor que cem");
+}else{
+    console.log(" esse numero é igual a cem");
+}
